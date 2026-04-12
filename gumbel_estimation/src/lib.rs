8efbfd13504dc03c19c0a51a45cafdee103b49bb
Paweł Leśniak <268419@student.pwr.edu.pl>
2026-04-12 10:34:27 +0200

    Add GHLL wrappers for geo/har means

--- gumbel_estimation/src/lib.rs
+++ gumbel_estimation/src/lib.rs
@@ -1,3 +1,5 @@
+use std::hash::{BuildHasher, Hash};
+
 mod common;
 mod registers;
 
@@ -6,7 +8,42 @@ pub mod ghll;
 pub mod ghll_real;
 pub mod ghll_plus;
 
-pub use gumbel::{GumbelTransform, ICDFGumbel, BitHackGumbel};
 pub use ghll::GHLL;
 pub use ghll_real::GHLLReal;
+
+pub use gumbel::{GumbelTransform, ICDFGumbel, BitHackGumbel};
 pub use ghll_plus::GHLLPlus;
+
+pub trait GumbelEstimator<T: ?Sized> {
+    fn add(&mut self, value: &T);
+    fn count(&self) -> f64;
+}
+
+pub struct GHLLGeo<B: BuildHasher, G: GumbelTransform>(GHLL<B, G>);
+impl<B: BuildHasher, G: GumbelTransform, T: Hash> GumbelEstimator<T> for GHLLGeo<B, G> {
+    fn add(&mut self, value: &T) { self.0.add(value); }
+    fn count(&self) -> f64 { self.0.count_geo() }
+}
+
+pub struct GHLLHar<B: BuildHasher, G: GumbelTransform>(GHLL<B, G>);
+impl<B: BuildHasher, G: GumbelTransform, T: Hash> GumbelEstimator<T> for GHLLHar<B, G> {
+    fn add(&mut self, value: &T) { self.0.add(value); }
+    fn count(&self) -> f64 { self.0.count_har() }
+}
+
+pub struct GHLLRealGeo<B: BuildHasher, G: GumbelTransform>(GHLLReal<B, G>);
+impl<B: BuildHasher, G: GumbelTransform, T: Hash> GumbelEstimator<T> for GHLLRealGeo<B, G> {
+    fn add(&mut self, value: &T) { self.0.add(value); }
+    fn count(&self) -> f64 { self.0.count_geo() }
+}
+
+pub struct GHLLRealHar<B: BuildHasher, G: GumbelTransform>(GHLLReal<B, G>);
+impl<B: BuildHasher, G: GumbelTransform, T: Hash> GumbelEstimator<T> for GHLLRealHar<B, G> {
+    fn add(&mut self, value: &T) { self.0.add(value); }
+    fn count(&self) -> f64 { self.0.count_har() }
+}
+
+impl<B: BuildHasher, G: GumbelTransform, T: Hash> GumbelEstimator<T> for GHLLPlus<B, G> {
+    fn add(&mut self, value: &T) { self.add(value); }
+    fn count(&self) -> f64 { self.count() }
+}
