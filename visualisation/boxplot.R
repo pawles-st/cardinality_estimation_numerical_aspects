94b36c7444dbafbfc27f7a5b1a5bd9ea9e1a392f
Paweł Leśniak <268419@student.pwr.edu.pl>
2026-04-22 15:52:50 +0200

    Update boxplot look and print extra results

--- visualisation/boxplot.R
+++ visualisation/boxplot.R
@@ -22,25 +22,35 @@ gumbel.algs <- c(
 
 transforms <- c("ICDF", "SimpleBitHack", "TaylorBitHack")
 
-# plot generation
+# Plot generation
 
-for (trans in transforms) {
-	algs <- c(names(base.algs), paste0(names(gumbel.algs), "_", trans))
-	algs.readable <- c(unname(base.algs), paste0(unname(gumbel.algs), "(", trans , ")"))
-	no.algs <- length(algs)
-
-	for (prec in precisions) {
+for (prec in precisions) {
+	for (trans in transforms) {
+		algs <- c(names(base.algs), paste0(names(gumbel.algs), "_", trans))
+		algs.readable <- c(unname(base.algs), paste0(unname(gumbel.algs), "(", trans , ")"))
+		no.algs <- length(algs)
 
 		estimates <- list()
 
-		# read all estimates
+		# Read all estimates
 
 		for (i in 1:no.algs) {
 			alg.estimates <- read.data(algs[i], prec, card, mult)
 			estimates <- c(estimates, list(alg.estimates / card))
 		}
 
-		# create a comparison boxplot
+		# Output statistics
+
+		cat(paste0("\n--- Statistics for transform: ", trans, ", precision: ", prec, " ---\n"))
+		for (i in 1:no.algs) {
+			m <- mean(estimates[[i]])
+			med <- median(estimates[[i]])
+			iqr_val <- IQR(estimates[[i]])
+			cat(sprintf("%-20s | Mean: %0.6f | Median: %0.6f | IQR: %0.6f\n", 
+						algs[i], m, med, iqr_val))
+		}
+
+		# Create a comparison boxplot
 
 		png(paste0("boxplot_", trans, "_", prec, ".png"), width = 1920, height = 1080)
 		
@@ -50,26 +60,39 @@ for (trans in transforms) {
 			main = paste0("Wykres pudełkowy dla algorytmów (n = ", format(card, scientific = FALSE), ", k = ", 2^prec , ", transformata = ", trans, ")"),
 			xlab = "",
 			ylab = "",
-			names = algs,
+			names = NA,
 			col = rainbow(no.algs),
 			cex.main = 3,
 			cex.axis = 1.5,
-			las = 2
+			xaxt = "n"
 		)
+		
+		# Draw x-axis without labels
+		axis(1, at = 1:no.algs, labels = FALSE)
+
+		# Add labels
+		axis.labels <- c(names(base.algs), names(gumbel.algs))
+		text(x = 1:no.algs, 
+			 y = par("usr")[3] - (par("usr")[4] - par("usr")[3]) * 0.03, 
+			 labels = axis.labels, 
+			 srt = 45, 
+			 adj = 1, 
+			 xpd = TRUE, 
+			 cex = 1.5)
 		
 		title(xlab = "Algorytm", line = 10, cex.lab = 3)
 		title(ylab = "Estymacja / Liczba unikalnych elementów", line = 3.5, cex.lab = 3)
 
-		# mark the means
+		# Mark the means
 
 		means <- lapply(estimates, mean)
 		points(unlist(means), pch = 3, cex = 2, lwd = 2)
 
-		# mark the cardinality with a line
+		# Mark the cardinality with a line
 
 		abline(h = 1, lwd = 2)
 
-		# add a legend
+		# Add a legend
 
 		legend("topleft",
 			   legend = algs.readable,
